721f2e1af086d7cfdec7c8534d838745c9fcb2f5
Jakub Vrchota <vrchota@webovy-servis.cz>
2022-12-18 13:31:25 +0100

    Vrácení předehřívací teploty hotendu na 190

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1874,7 +1874,7 @@
 // Preheat Constants - Up to 5 are supported without changes
 //
 #define PREHEAT_1_LABEL       "PLA"
-#define PREHEAT_1_TEMP_HOTEND 170
+#define PREHEAT_1_TEMP_HOTEND 190
 #define PREHEAT_1_TEMP_BED     60
 #define PREHEAT_1_TEMP_CHAMBER 35
 #define PREHEAT_1_FAN_SPEED     0 // Value from 0 to 255
